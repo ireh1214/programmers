ecf78a119d55920f7d82f86cb18d08ca8508cf0b
ireh1214 <80033166+ireh1214@users.noreply.github.com>
2024-07-15 15:41:43 +0900

    [level 1] Title: 모의고사, Time: 3.70 ms, Memory: 35 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,31 +1,28 @@
 function solution(answers) {
-    const num1 = [1,2,3,4,5]
-    const num2 = [2,1,2,3,2,4,2,5]
-    const num3 = [3,3,1,1,2,2,4,4,5,5]
-    
-    const sum = [0, 0, 0];
-    
-    for (const [i,x] of answers.entries()){
-        if (x === num1[i% num1.length] ){
-            sum[0]++
-        }
-                if (x === num2[i% num2.length]){
-            sum[1]++
-        }
-                if (x === num3[i% num3.length]){
-            sum[2]++
-        }  
-    }
+	const n1 = [1, 2, 3, 4, 5];
+	const n2 = [2, 1, 2, 3, 2, 4, 2, 5];
+	const n3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+	const sum = [0, 0, 0];
 
-    const max = Math.max(...sum)
-    const result = []
-    
-    for (const [y,z] of sum.entries()){
-        if (z === max){
-            result.push(y + 1)
-        }
-    }
-    
-    
-	return result
+	for (const [i, x] of answers.entries()) {
+		if (x === n1[i % n1.length]) {
+			sum[0]++;
+		}
+		if (x === n2[i % n2.length]) {
+			sum[1]++;
+		}
+		if (x === n3[i % n3.length]) {
+			sum[2]++;
+		}
+	}
+
+	const result = [];
+
+	for (const [i, x] of sum.entries()) {
+		if (x === Math.max(...sum)) {
+			result.push(i + 1);
+		}
+	}
+
+	return result;
 }
